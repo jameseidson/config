27ac3709263bf6c9d6ff1bd161709fe0984ed93b
James Eidson <eidsonje@icloud.com>
2022-10-19 11:25:15 -0500

    fish: fixed ssh abbr and added rmswap

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-abbr -a -U ssh ssh TERM=xterm-256color ssh
+abbr -a -U ssh "TERM=xterm-256color ssh" 
 abbr -a -U new "alacritty &> /dev/null & disown"
+abbr -a -U rmswap "rm -rf ~/.local/state/nvim/swap"
